96368d95f86ad02715f7a104f8923012bbed4512
xxmm717 <zhangxuemian2019@outlook.com>
2026-08-03 16:48:09 +0800

    feat: 初始化项目基础环境与 Nacos 认证配置
    - 各微服务 Nacos 注册中心、配置中心地址调整为 192.168.246.128:8848
    - 为 auth、gateway、system、product、order、payment 等所有服务 bootstrap.yml 补充 Nacos 登录认证
    - 网关补充 Sentinel Nacos 数据源账号密码认证
    - 调整 IpUtils 本地 IP 与内网 IP 判断为 192.168.246.128
    - 调整渠道服务线程池核心线程初始化说明

--- spzx-common/spzx-common-core/src/main/java/com/spzx/common/core/utils/ip/IpUtils.java
+++ spzx-common/spzx-common-core/src/main/java/com/spzx/common/core/utils/ip/IpUtils.java
@@ -65,7 +65,7 @@ public class IpUtils
             ip = request.getRemoteAddr();
         }
 
-        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : getMultistageReverseProxyIp(ip);
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "192.168.246.128" : getMultistageReverseProxyIp(ip);
     }
 
     /**
@@ -77,7 +77,7 @@ public class IpUtils
     public static boolean internalIp(String ip)
     {
         byte[] addr = textToNumericFormatV4(ip);
-        return internalIp(addr) || "127.0.0.1".equals(ip);
+        return internalIp(addr) || "192.168.246.128".equals(ip);
     }
 
     /**
@@ -225,7 +225,7 @@ public class IpUtils
         catch (UnknownHostException e)
         {
         }
-        return "127.0.0.1";
+        return "192.168.246.128";
     }
 
     /**
